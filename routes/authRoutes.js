fc481455bf5ffa09b87b225d23d28f85cc3fd55d
Ornella B <borges.ornella@gmail.com>
2023-08-30 14:23:53 +0200

    save

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,3 +11,11 @@
 const withAuth = require("../withAuth");
 const jwt = require("jsonwebtoken");
 const secret = "popo";
+
+module.export = (app, db) => {
+  const userModel = require("../models/userModel")(db);
+
+  // route de recup des infos de l'user par son token (permet la reconxion automatique du front)
+  // de que le front demande la reconnexion via la requette l'api verifie le token et si il
+  // correspond bien il va retourner les datas au front
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,26 +1,26 @@
-
-
-
-
-
 // CREATION DES ROUTES QUI PERMETTENT DE RECEPTIONNER LES REQUETES (marchandises qui arrive sur les rail)
 
 module.exports = (app, db) => {
-    const userModel = require("../models/userModel")(db);
+  const userModel = require("../models/userModel")(db);
 
-    //ROUTE TEST
-    app.get('/api/testUser', async(req, res, next) => {
-        console.log('req',req)
-        res.json({msg: 'tout va vien bro, la route testUser fonctionne'})
-    })
+  //ROUTE TEST
+  app.get("/api/testUser", async (req, res, next) => {
+    console.log("req", req);
+    res.json({ msg: "tout va vien bro, la route testUser fonctionne" });
+  });
 
-    //Route de creation d'une annonce
-    app.post('/api/user/createAccount', async(req, res, next) => {
-        
-        // ici on stock dans la constante une le resultat de la requete sql qui se trouve dans le model saveUser
-        const result = await userModel.saveUser(req)
+  //Route de creation d'une annonce
+  app.post("/api/user/createAccount", async (req, res, next) => {
+    // ici on stock dans la constante une le resultat de la requete sql qui se trouve dans le model saveUser
+    const result = await userModel.saveUser(req);
 
-        res.json(result) // reponse pour le front qu'on convertit en json
-    })
+    // reponse pour le front qu'on convertit en json le result
+    res.json(result);
+  });
 
-}
+  app.put("/api/user/updateAccount", async (req, res, next) => {});
+
+  app.post("/api/user/deleteAccount", async (req, res, next) => {});
+
+  app.post("/api/user/logginAccount", async (req, res, next) => {});
+};

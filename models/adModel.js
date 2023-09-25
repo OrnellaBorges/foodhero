5b2a20bb08478329672610a4d6f202631e6f5d3c
Ornella B <borges.ornella@gmail.com>
2023-09-25 22:25:50 +0200

    init adModel

--- models/adModel.js
+++ models/adModel.js
@@ -4,19 +4,75 @@ module.exports = (_db) => {
 };
 
 class AdModel {
-    //récupération des annonces
-
-    static getAllAds() {}
+    //récupération de toutes les annonces
+    static getAllAds() {
+        return db
+            .query("SELECT * FROM ads")
+            .then((res) => {
+                return res;
+            })
+            .catch((err) => {
+                return err;
+            });
+    }
 
     // recup d'une seule annonce par son id
-    static getOneAd(id) {}
+    static getOneAd(id) {
+        return db.query("SELECT * FROM ads WHERE id = ?", [id]).then().catch();
+    }
 
     // sauvegarde d'une annonce
-    static saveOneAd(id) {}
+    static saveOneAd(id) {
+        return db
+            .query(
+                "INSERT INTO ads (id, title, price, photo, quantity, creationTimestamp) VALUES (?, ?, ?, ?, ?, NOW())",
+                [
+                    req.body.name,
+                    req.body.description,
+                    req.body.price,
+                    req.body.photo,
+                    req.body.quantity,
+                ]
+            )
+            .then((res) => {
+                return res;
+            })
+            .catch((err) => {
+                return err;
+            });
+    }
 
     // modification d'une annonce
-    static updateOneAd(req, id) {}
+    static updateOneAd(req, id) {
+        return db
+            .query(
+                "UPDATE beers SET name=? , description=?, price=?, photo=?, quantity=? WHERE id=?",
+                [
+                    req.body.name,
+                    req.body.description,
+                    req.body.price,
+                    req.body.photo,
+                    req.body.quantity,
+                    id,
+                ]
+            )
+            .then((res) => {
+                return res;
+            })
+            .catch((err) => {
+                return err;
+            });
+    }
 
     // supprimer une annonce
-    static deleteOneAd() {}
+    static deleteOneAd(id) {
+        return db
+            .query("DELETE FROM ads WHERE id=?", [id])
+            .then((response) => {
+                return response;
+            })
+            .catch((err) => {
+                return err;
+            });
+    }
 }

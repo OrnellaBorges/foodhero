d4aa612ee59cb74006e8c6e2b649a340d5f4b31d
Ornella B. <borges.ornella@gmail.com>
2023-10-14 19:36:09 +0200

    Routes du user ok

--- models/userModel.js
+++ models/userModel.js
@@ -11,50 +11,43 @@ class UserModel {
     //SAUVEGARDE UTILISATEUR
     // static est une fonction version class en poo
     // en argument on passe req qui vient du front ce sont les donnée que l'utilisateur rentre lorsqyu'il ecrit dans les formulaires pour creer un compte
-    static async saveOneUser(req) {
+    static async createOneUser(req) {
         //ici on crée des racourcis des datas reçues de l'utilisateur qui sont dans req.body
-        /* const password = req.body.password
-    const firstName = req.body.firstName
-    const lastName = req.body.lastName
-    const email = req.body.email */
 
         // ici on fait un racourci on destructure et on affecte req.body
-        const { password, email } = req.body;
+        const { password, email, firstName, lastName } = req.body;
 
         // verifier si le mail est utiliser ou pas on fait une requete sql qui cherche dans tous lers users
-        const user = await db.query("SELECT * FROM user WHERE email = ?", [
+        /* const user = await db.query("SELECT * FROM user WHERE email = ?", [
             email,
         ]);
-        //console.log('user', user)
 
         // si il y a un utilisateur avec le mail fourni en req alors
         if (user.length) {
             // on envoit un message d'erreur
             return "email déjà utilisé";
-        } else {
-            //ici on stock dans une constante  et on crypte le mot de passe
-            const cryptedPassword = await bcrypt.hash(password, saltRounds);
+        } else { */
+        //ici on stock dans une constante  et on crypte le mot de passe
+        const cryptedPassword = await bcrypt.hash(password, saltRounds);
 
-            // on enregistre vraiment l'utilisateur si il n'ay a pas l'email en bdd
-            return (
-                db
-                    //.query() est une methode qui permet de faire la requete sql entre les parenthèses
-                    .query(
-                        "INSERT INTO users (email, cryptedPassword, accountCreationDate) VALUES(?, ?, ?, ?, NOW())",
-                        [email, cryptedPassword]
-                    )
-                    //en cas de succès il retourne les données
-                    .then((result) => {
-                        console.log("result", result);
-                        return result;
-                    })
-                    .catch((err) => {
-                        console.log("err", err);
-                        return err;
-                        // si il ne trouve pas il renvoit l'erreur
-                    })
-            );
-        }
+        // on enregistre vraiment l'utilisateur si il n'y a pas l'email en bdd
+        return (
+            db
+                //.query() est une methode qui permet de faire la requete sql entre les parenthèses
+                .query(
+                    "INSERT INTO users (firstName, lastName, email, cryptedPassword, accountCreationDate) VALUES(?, ?, ?, ?, NOW())",
+                    [firstName, lastName, email, cryptedPassword]
+                )
+                //en cas de succès il retourne les données
+                .then((result) => {
+                    return result;
+                })
+                .catch((err) => {
+                    return err;
+                    // si il ne trouve pas il renvoit l'erreur
+                })
+        );
+        //}
     }
 
     //ici on recup un utilisateur par son email donc on lui passe en paramètre de la fonction email
@@ -62,12 +55,10 @@ class UserModel {
         return db
             .query("SELECT * FROM users WHERE email = ?", [email])
             .then((response) => {
-                console.log("response getOneUserByEmail", response);
                 return response;
                 //en cas de succès il retourne les données
             })
             .catch((err) => {
-                console.log("err", err);
                 return err;
                 // si il ne trouve pas il renvoit l'erreur
             });
@@ -76,43 +67,49 @@ class UserModel {
     //ici on recup un utilisateur par son id donc on a besoin de lui passer en argument "id" car il a besoin de cet data qui vien du front
     static getOneUserById(id) {
         return db
-            .query("SELECT * FROM users WHERE id=?", [id])
+            .query("SELECT * FROM users WHERE id = ?", [id])
             .then((response) => {
                 // dans le cas ou il y a les données il va renvoyer les données via response
-                console.log("response1", response);
                 return response;
             })
 
             .catch((err) => {
                 //si il il y a un problème dans la bdd il va revoyer l'erreur
-                console.log("err", err);
                 return err;
             });
     }
 
     // MODIFIER UTILISATEUR : en paramettre de la fonction on a besoin de la requete du front et de id de l'utilisateur et la require (requete)
-    static updateOneUser(req, id) {
-        console.log("est quon rentre ici");
-
-        console.log("req.body", req.body);
-        console.log("id", id);
-
-        let { firstName, lastName, address, zip, city, phoneNumber } = req.body;
-
-        /* if(firstName === undefined) firstName = ''
-        if(lastName === undefined) firstName = ''
-        if(addressName === undefined) firstName = ''
-        if(zip === undefined) firstName = ''
-        if(city === undefined) firstName = ''
-        if(phone === undefined) firstName = '' */
-
+    static async updateOneUser(req, id) {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            phoneNumber,
+            pseudo,
+            address,
+            city,
+            zip,
+        } = req.body;
+        const cryptedPassword = await bcrypt.hash(password, saltRounds);
         return (
             db
                 .query(
-                    "UPDATE users SET firstName = ?, lastName = ?, address = ?, zip = ?, city = ?, phoneNumber = ? WHERE id = ?",
-                    [firstName, lastName, address, zip, city, phoneNumber, id]
+                    "UPDATE users SET firstName = ?, lastName = ?, email = ?, cryptedPassword = ?, phoneNumber = ?, pseudo = ?, address = ?, city = ?, zip = ? WHERE id = ?",
+                    [
+                        firstName,
+                        lastName,
+                        email,
+                        cryptedPassword,
+                        phoneNumber,
+                        pseudo,
+                        address,
+                        city,
+                        zip,
+                        id,
+                    ]
                 )
-
                 .then((response) => {
                     // on passe en argument la response de la requete
                     // en cas de succès on retourne la response donc toutes les données demandées dans la requete
@@ -127,7 +124,7 @@ class UserModel {
     }
 
     //Mise a jour de la connexion => dernière date de connexion a afficher par exemple
-    static updateConnexion(id) {
+    /* static updateConnexion(id) {
         //const { id } = req.params;
         return db
             .query("UPDATE users SET connexionTimestamp = NOW() WHERE id = ?", [
@@ -139,7 +136,7 @@ class UserModel {
             .catch((err) => {
                 return err;
             });
-    }
+    } */
 
     //SUPPRIMER UN COMPTE
     static deleteOneUser(id) {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,56 +1,50 @@
 // CREATION DES ROUTES QUI PERMETTENT DE RECEPTIONNER LES REQUETES (marchandises qui arrive sur les rail)
-
+const bcrypt = require("bcryptjs");
 const withAuth = require("../withAuth");
+const config = require("../config.js");
+const secret = process.env.SECRET || config.token.secret;
+const jwt = require("jsonwebtoken");
 
 module.exports = (app, db) => {
     // sert a exporter le fichier
     const UserModel = require("../models/userModel")(db);
 
-    //ROUTE TEST
-    app.get("/api/v1/user/test", async (req, res, next) => {
-        console.log("req", req);
-        res.json({ msg: "tout va vien bro, la route testUser fonctionne" });
-    });
-
     //CREATE = creation d'un compte utilisateur
     //Route de creation d'un utilisateur
-    app.post("/api/v1/user/create", withAuth, async (req, res, next) => {
+    app.post("/api/v1/user/create", async (req, res, next) => {
         const { email } = req.body;
         // 1- on verifie si l'utilisateur existe déjà ou pas si il existe on le refoule.
-        // on stock dans une constante let check le resultat de la fonction getOneUserByEmail qui recup un utilisateur par son email
-        const check = await UserModel.getOneUserByEmail(email); // ici on stock dans la constante toute les données reçu de la fonction getOneUserByEmail qui fait une requete a la bdd dans le UserModel
-        console.log("check", check);
+        // on stock dans une constante let userFound le resultat de la fonction getOneUserByEmail qui recup un utilisateur par son email
+        const userFound = await UserModel.getOneUserByEmail(email); // ici on stock dans la constante toute les données reçu de la fonction getOneUserByEmail qui fait une requete a la bdd dans le UserModel
         //ici on fait if / else une condition si ???
-        if (check.code) {
-            // si il une err dans la requete sql il va stocker un objet d'erreur dans la variable check
-            console.log("check2", check);
-            // check doit retourner un tableau vide pour signifier que la place est libre pour cette utilisateur et personne n'utilise le meme email
-            //console.log("check2", check);
+        if (userFound.code) {
+            // si il une err dans la requete sql il va stocker un objet d'erreur dans la variable userFound
+            // userFound doit retourner un tableau vide pour signifier que la place est libre pour cette utilisateur et personne n'utilise le meme email
             res.json({
                 status: 500,
-                msg: "Erreur vérification email",
-                err: check,
+                msg: "pb dans la requete sql de getOneUserById",
+                err: userFound.code,
             });
         } else {
-            // si check voit qu'il y a qq1 avec le meme mail il retourne un tableau qui n'est pas vide et en front le res.json
+            // si userFound voit qu'il y a qq1 avec le meme mail il retourne un tableau qui n'est pas vide et en front le res.json
 
-            if (check.length > 0) {
+            if (userFound.length) {
                 // si le tableau est supérieur a 0
-                if (check[0].email === email) {
-                    // CE IF NA PAS LIEU DETRE !!!!
-                    // et que check.email correspondent alors on envoit le msg au front "Email déjà utilisé."
-                    res.json({ status: 401, msg: "Email déjà utilisé." });
-                }
+                //if (userFound[0].email === email) {
+                // CE IF NA PAS LIEU DETRE !!!!
+                // et que userFound.email correspondent alors on envoit le msg au front "Email déjà utilisé."
+                res.json({ status: 401, msg: "Email déjà utilisé." });
+                //}
             } else {
                 // dans le cas ou c'est inférieur à 0 c'est que la place est libre
-                // on stock dans let user la reponse de la fonction de sauvegarde d'un utilisateur saveOneUser(req) on lui passe la req du front en argument
-                let user = await UserModel.saveOneUser(req);
-                if (user.code) {
+                // on stock dans let user la reponse de la fonction de sauvegarde d'un utilisateur createOneUser(req) on lui passe la req du front en argument
+                const userCreated = await UserModel.createOneUser(req);
+                if (userCreated.code) {
                     // ???? ERROR DANS LA REQUETE MAIS IL NE RENTRE JAMAIS MDR POURQUOI
                     res.json({
                         status: 500,
-                        msg: "Il y a eu un problème",
-                        err: user,
+                        msg: "pb dans la requete sql de createOneUser",
+                        err: userCreated.code,
                     });
                 } else {
                     // QUAND TOUT EST OK
@@ -63,45 +57,41 @@ module.exports = (app, db) => {
         }
     });
 
-    /* // ENREGISTREMENT NOUVEL UTILISATEUR SI IL EXISTE PAS
-    // ici on stock dans la constante une le resultat de la requete sql qui se trouve dans le model saveUser
-    const result = await UserModel.saveOneUser(req);
-    console.log("result saveOneUser", result);
-
-    // reponse pour le front qu'on convertit en json le result
-    res.json(result);
-  }); */
-
     //UPDATE = modifier un compte
     app.put("/api/v1/user/update/:id", async (req, res, next) => {
         //on stock dans une constante le resultat de la requete updateOneUser qui se trouve dans la class UserModel
         // on doit lui passer en argument le req du front et l'id
         const { id } = req.params;
-
-        let user = await UserModel.updateOneUser(req, id);
-
+        const userFound = await UserModel.getOneUserById(id);
         //VERIFICATION si la requete sql à echoué avec une condition avec le .code qui est un objet d'erreur
-        if (user.code) {
-            //on revoi une reponse au front qu'on convertir en json
+        if (userFound.code) {
             res.json({
                 status: 500,
-                msg: "Gros problème dans la requete sql tu dois vérifier la syntaxe de la requete dans UserModel",
-                err: user,
-            });
-        }
-
-        // PROFIL MODIFIE => renvoyer info mis a jour au front => redux peut mettre a jour automatiquement les info de l'utilisateur connecté
-
-        //stocker dans newUser les donnée que donne getOneUser
-        let newUser = await UserModel.getOneUser(id);
-        if (user.code) {
-            res.json({
-                status: 500,
-                msg: "pb dans la requete sql",
-                err: newUser,
+                msg: "pb dans la requete sql de getOneUserById",
+                err: userFound.code,
             });
         } else {
-            res.json({ status: 500, result: user, newUser: newUser[0] });
+            if (userFound.length) {
+                const userUpdated = await UserModel.updateOneUser(req, id);
+                if (userUpdated.code) {
+                    res.json({
+                        status: 500,
+                        msg: "pb dans la requete sql de updateOneUser",
+                        err: userUpdated.code,
+                    });
+                } else {
+                    res.json({
+                        status: 200,
+                        result: "utilisateur bien modifié",
+                    });
+                }
+            } else {
+                res.json({
+                    status: 500,
+                    msg: "L'utilisateur n'existe pas",
+                    err: userFound.code,
+                });
+            }
         }
     });
 
@@ -110,86 +100,108 @@ module.exports = (app, db) => {
     app.post("/api/v1/user/login", async (req, res, next) => {
         //version ES6
         // ici on fait une DESTRUCTURATION en ES6 qui permet d'eviter d'ecrire req.body.email
-        const { email } = req.body; // on a besoin de l'email rentré par l'utilisateur
+        const { email, password } = req.body; // on a besoin de l'email rentré par l'utilisateur
         //version ES5 on faisait comme ça
         //const email = req.body.email
-
-        // VERIFICATIONS:
-
-        if (email === "") {
+        if (email === "" || password === "") {
             // SI UTILISATEUR NA RIEN MIS DANS L4INPUT
             //LE RES ON LE CONSTRUIT COMME ON VEUT C4EST A NOUS DE LA FAIRE
             // si l'email est strictement egal a une chaine de caractère vide
             // res est la reponse que l'on va renvoyer au front
             //.json permet de convertir la res (response) au format json
             // Dans les parenthèses on ecrit ou on invente la response qui sera reçu par le front en format json
-            res.json({ status: 401, msg: "Enter your email..." });
-        }
-        // VERIFICATION = si l'utilisateur existe dans la bdd avec un email correspondant
-        // on utilise la fonction .getOneUserByEmail(email) on passe en argument l'email de l'utilisateur et la fonction du Model va chercher l'email via la requerte sql dela query
-        let user = await UserModel.getOneUserByEmail(email);
-        //si user.code existe on renvoit une response 500 au front
-        if (user.code) {
-            // cas 1: si il existe et on fait un message d'erreur au front
             res.json({
-                status: 500,
-                msg: "Erreur vérification email.",
-                err: user,
+                status: 401,
+                msg: "Enter your email or your password...",
             });
-        }
-
-        if (user.lenght === 0) {
-            //cas 2 :  SI IL N'EXISTE PAS DANS BDD = L'utilisateur a rentrée un mail mais il s'est trompé
-            //on retourne un message d'erreur
-            res.json({
-                status: 404,
-                msg: "Pas d'utilisateur correspondant à ce mail.",
-            });
-        }
-        //on compare les password avec bcrypt
-        bcrypt
-            .compare(req.body.password, user[0].password)
-            .then((same) => {
-                //si c'est true
-                if (same) {
-                    //dans payload on stock les valeurs qu'on va glisser dans le token (attention jamais d'infos craignos!)
-                    const payload = { email: req.body.email, id: user[0].id };
-                    //on crée notre token avec sa signature (secret)
-                    const token = jwt.sign(payload, secret);
-                    console.log("token", token);
-
+        } else {
+            // VERIFICATION = si l'utilisateur existe dans la bdd avec un email correspondant
+            // on utilise la fonction .getOneUserByEmail(email) on passe en argument l'email de l'utilisateur et la fonction du Model va chercher l'email via la requerte sql dela query
+            //si user.code existe on renvoit une response 500 au front
+            const user = await UserModel.getOneUserByEmail(email);
+            if (user.code) {
+                // cas 1: si il existe et on fait un message d'erreur au front
+                res.json({ status: 500, error: user.code });
+            } else {
+                if (user.length === 0) {
+                    //cas 2 :  SI IL N'EXISTE PAS DANS BDD = L'utilisateur a rentrée un mail mais il s'est trompé
+                    //on retourne un message d'erreur
                     res.json({
-                        status: 200,
-                        token: token,
-                        user_id: user[0].id,
+                        status: 404,
+                        msg: "Pas d'utilisateur correspondant à ce mail.",
                     });
                 } else {
-                    //on retourne un json d'erreur
-                    res.json({
-                        status: 401,
-                        error: "Votre mot de passe est incorrect !",
-                    });
+                    //on compare les password avec bcrypt
+                    const same = await bcrypt.compare(
+                        password,
+                        user[0].cryptedPassword
+                    );
+                    //si c'est true
+                    if (same) {
+                        //dans payload on stock les valeurs qu'on va glisser dans le token (attention jamais d'infos craignos!)
+                        const payload = { email: email, id: user[0].id };
+                        //on crée notre token avec sa signature (secret)
+                        const token = jwt.sign(payload, secret);
+                        res.json({
+                            status: 200,
+                            token: token,
+                            user: user[0],
+                            msg: "Connecté",
+                        });
+                    } else {
+                        //on retourne un json d'erreur
+                        res.json({
+                            status: 401,
+                            error: "Votre mot de passe est incorrect",
+                        });
+                    }
                 }
-            })
-            .catch((err) => console.log(err));
+            }
+        }
     });
 
     // LOGOUT = route qui permet a l'utilisateur de se déconnecter
 
     //DELETE = supprimer le compte d'un compte
-    app.delete("/api/v1/user/delete/:id", withAuth, async (req, res, next) => {
+    app.delete("/api/v1/user/delete/:id", async (req, res, next) => {
         const { id } = req.params;
-        //
+        const userFound = await UserModel.getOneUserById(id);
+        if (userFound.code) {
+            res.json({
+                status: 500,
+                msg: "pb dans la requete sql de getOneUserById",
+                err: userFound.code,
+            });
+        } else {
+            if (userFound.length) {
+                const userDeleted = await UserModel.deleteOneUser(id);
+                if (userDeleted.code) {
+                    res.json({
+                        status: 500,
+                        msg: "pb dans la requete sql de deleteOneUser",
+                        err: userDeleted.code,
+                    });
+                } else {
+                    res.json({
+                        status: 200,
+                        result: "Votre compte à bien été supprimé.",
+                    });
+                }
+            } else {
+                res.json({
+                    status: 500,
+                    msg: "L'utilisateur n'existe pas",
+                    err: userFound.code,
+                });
+            }
+        }
 
-        let reponse = await UserModel.deleteOneUser(id);
-        console.log("reponse.affectedRows", reponse.affectedRows); //=> on reçoit 1 ou 0 dans le terminnal apres avoir fait une requete via postman
-        console.log("reponse.code", reponse.code);
-        const deleteSuccess = reponse.affectedRows; //
+        /* const deleteSuccess = reponse.affectedRows;
         if (deleteSuccess) {
             res.json({ status: 200, msg: "Votre compte à bien été supprimé." });
         } else {
             res.json({ status: 500, msg: "il y a un soucis !" });
-        }
+        } */
         /* // si la requete sql est mal ecrite
         if (user.code) {
             // . code permet de generer un objet d'erreur si celui ci existe alors c'est que la requete sql est mauvaise

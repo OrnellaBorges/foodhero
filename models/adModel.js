15078c52f2bae79e422fa4ab2dec0c118501f9e7
Ornella B <borges.ornella@gmail.com>
2023-10-26 23:43:09 +0200

    add api files

--- models/adModel.js
+++ models/adModel.js
@@ -1,88 +1,91 @@
 module.exports = (_db) => {
-    db = _db;
-    return AdModel;
+  db = _db;
+  return AdModel;
 };
 
 class AdModel {
-    // sauvegarde/ creation d'une annonce quand l'user veut creer une annonce via un formulaire
-    static createOneAd(req) {
-        // req.body contient tous les champs du formulaire
-        //ici on fait une destructuration ce qui permet de racourcir et d'eviter req.body.xxx
-        const { userId, title, description, price } = req.body;
-        return db
-            .query(
-                "INSERT INTO ads (title, description, creationDate, userId, price) VALUES (?, ?, NOW(), ?, ?)",
-                [title, description, userId, price]
-            )
-            .then((res) => {
-                return res;
-            })
-            .catch((err) => {
-                return err;
-            });
-    }
+  // sauvegarde/ creation d'une annonce quand l'user veut creer une annonce via un formulaire
+  static createOneAd(req) {
+    // req.body contient tous les champs du formulaire
+    //ici on fait une destructuration ce qui permet de racourcir et d'eviter req.body.xxx
+    const { userId, title, description, price } = req.body;
 
-    //récupération de toutes les annonces
-    static getAllAds() {
-        return db
-            .query("SELECT * FROM ads")
-            .then((res) => {
-                return res;
-            })
-            .catch((err) => {
-                return err;
-            });
-    }
+    return db
+      .query(
+        "INSERT INTO ads (title, description, creationDate, userId, price) VALUES (?, ?, NOW(), ?, ?)",
+        [title, description, userId, price]
+      )
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
 
-    // recup d'une seule annonce par son id >> quan dl'user va cliquer sur le détail d'une annonce
-    static getOneAd(id) {
-        return db
-            .query("SELECT * FROM ads WHERE id = ?", [id])
-            .then((res) => {
-                return res;
-            })
-            .catch((err) => {
-                return err;
-            });
-    }
+  //récupération de toutes les annonces
+  static getAllAds() {
+    return db
+      .query("SELECT * FROM ads")
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
 
-    // modification d'une annonce
-    static updateOneAd(req, id) {
-        const { title, description, price } = req.body;
+  // recup d'une seule annonce par son id >> quan dl'user va cliquer sur le détail d'une annonce
+  static getOneAd(id) {
+    return db
+      .query("SELECT * FROM ads WHERE id = ?", [id])
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
 
-        return db
-            .query(
-                "UPDATE ads SET title=? , description=?, price=? WHERE id=?",
-                [title, description, price, id]
-            )
-            .then((res) => {
-                return res;
-            })
-            .catch((err) => {
-                return err;
-            });
-    }
+  // modification d'une annonce
+  static updateOneAd(req, id) {
+    const { title, description, price } = req.body;
 
-    // supprimer une annonce
-    static deleteOneAd(id) {
-        return db
-            .query("DELETE FROM ads WHERE id=?", [id])
-            .then((res) => {
-                return res;
-            })
-            .catch((err) => {
-                return err;
-            });
-    }
+    return db
+      .query("UPDATE ads SET title=? , description=?, price=? WHERE id=?", [
+        title,
+        description,
+        price,
+        id,
+      ])
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
 
-    static getUserAds(userId) {
-        return db
-            .query("SELECT * FROM ads WHERE userId = ?", [userId])
-            .then((res) => {
-                return res;
-            })
-            .catch((err) => {
-                return err;
-            });
-    }
+  // supprimer une annonce
+  static deleteOneAd(id) {
+    return db
+      .query("DELETE FROM ads WHERE id=?", [id])
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
+
+  static getUserAds(userId) {
+    return db
+      .query("SELECT * FROM ads WHERE userId = ?", [userId])
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
 }
